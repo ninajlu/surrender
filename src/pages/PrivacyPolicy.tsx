14bd87088bd92af77cf30726c5cfa58d1a50c0ed
Nina Lu <ninajlu@gmail.com>
2025-01-19 00:16:02 -0800

    fixed5

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -16,10 +16,10 @@ export default function PrivacyPolicy() {
       </nav>
 
       <main className="container mx-auto px-4 py-12 max-w-3xl">
-        <h1 className="text-4xl font-bold mb-4 text-gray-900">Privacy Policy</h1>
+        <h1 className="text-4xl font-extrabold mb-4 text-gray-900">Privacy Policy</h1>
         <p className="text-gray-500 mb-12 text-lg">Last updated: March 19, 2024</p>
 
-        <div className="prose prose-lg max-w-none">
+        <div className="prose prose-lg max-w-none prose-headings:font-bold">
           <p>
             Surrender was built as a Free app. This SERVICE is provided at no cost and is intended for use as is.
           </p>
@@ -35,30 +35,71 @@ export default function PrivacyPolicy() {
             We will not use or share your information with anyone except as described in this Privacy Policy.
           </p>
 
-          <h2>Information Collection and Use</h2>
+          <p>
+            The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, unless 
+            otherwise defined in this Privacy Policy.
+          </p>
+
+          <h2 className="text-2xl font-extrabold text-gray-900 mt-8 mb-4">Information Collection and Use</h2>
           <p>
             For a better experience, while using our Service, we may require you to provide us with certain 
             personally identifiable information, including but not limited to name. The information that we request 
             will be retained by us and used as described in this privacy policy.
           </p>
 
-          <h2>Log Data</h2>
+          <h2 className="text-2xl font-extrabold text-gray-900 mt-8 mb-4">Service Providers</h2>
           <p>
-            We want to inform you that whenever you use our Service, in a case of an error in the app we collect 
-            data and information (through third party products) called Log Data. This Log Data may include 
-            information such as your device Internet Protocol ("IP") address, device name, operating system version, 
-            the configuration of the app when utilizing our Service, the time and date of your use of the Service, 
-            and other statistics.
+            We may employ third-party companies and individuals due to the following reasons:
+          </p>
+          <ul>
+            <li>To facilitate our Service;</li>
+            <li>To provide the Service on our behalf;</li>
+            <li>To perform Service-related services; or</li>
+            <li>To assist us in analyzing how our Service is used.</li>
+          </ul>
+          <p>
+            We want to inform users of this Service that these third parties have access to your Personal Information. 
+            The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to 
+            disclose or use the information for any other purpose.
           </p>
 
-          <h2>Security</h2>
+          <h2 className="text-2xl font-extrabold text-gray-900 mt-8 mb-4">Cookies</h2>
+          <p>
+            Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. 
+            These are sent to your browser from the websites that you visit and are stored on your device's internal memory.
+          </p>
+          <p>
+            This Service does not use these "cookies" explicitly. However, the app may use third-party code and libraries 
+            that use "cookies" to collect information and improve their services. You have the option to either accept or 
+            refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, 
+            you may not be able to use some portions of this Service.
+          </p>
+
+          <h2 className="text-2xl font-extrabold text-gray-900 mt-8 mb-4">Children's Privacy</h2>
+          <p>
+            These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable 
+            information from children under 13 years of age. In the case we discover that a child under 13 has provided us 
+            with personal information, we immediately delete this from our servers. If you are a parent or guardian and you 
+            are aware that your child has provided us with personal information, please contact us so that we will be able 
+            to take necessary actions.
+          </p>
+
+          <h2 className="text-2xl font-extrabold text-gray-900 mt-8 mb-4">Log Data</h2>
+          <p>
+            We want to inform you that whenever you use our Service, in a case of an error we collect data and 
+            information called Log Data. This Log Data may include information such as your device Internet Protocol 
+            ("IP") address, device name, operating system version, the configuration when utilizing our Service, 
+            the time and date of your use of the Service, and other statistics.
+          </p>
+
+          <h2 className="text-2xl font-extrabold text-gray-900 mt-8 mb-4">Security</h2>
           <p>
             We value your trust in providing us your Personal Information, thus we are striving to use commercially 
             acceptable means of protecting it. But remember that no method of transmission over the internet, or 
             method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.
           </p>
 
-          <h2>Links to Other Sites</h2>
+          <h2 className="text-2xl font-extrabold text-gray-900 mt-8 mb-4">Links to Other Sites</h2>
           <p>
             This Service may contain links to other sites. If you click on a third-party link, you will be directed 
             to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you 
@@ -66,7 +107,7 @@ export default function PrivacyPolicy() {
             the content, privacy policies, or practices of any third-party sites or services.
           </p>
 
-          <h2>Changes to This Privacy Policy</h2>
+          <h2 className="text-2xl font-extrabold text-gray-900 mt-8 mb-4">Changes to This Privacy Policy</h2>
           <p>
             We may update our Privacy Policy from time to time. Thus, you are advised to review this page 
             periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on 
@@ -75,7 +116,7 @@ export default function PrivacyPolicy() {
 
           <p>This policy is effective as of 2024-03-19</p>
 
-          <h2>Contact Us</h2>
+          <h2 className="text-2xl font-extrabold text-gray-900 mt-8 mb-4">Contact Us</h2>
           <p>
             If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at{' '}
             <a href="mailto:contact@surrender.app" className="text-green-600 hover:text-green-700">
